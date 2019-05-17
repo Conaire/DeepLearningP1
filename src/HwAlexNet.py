130fc2c69a21615091a8a795ff2a6d3e92cbc8e1
unknown <conorcl@hotmail.com>
2019-05-17 17:56:42 +0200

    uneeded files

--- src/HwAlexNet.py
+++ src/HwAlexNet.py
@@ -31,12 +31,16 @@ from tflearn.data_utils import samplewise_zero_center
 class HwAlexNet:
     @staticmethod
     def build(width, height, depth, classes):
+
+
+        activation = 'tanh'
+
         model = Sequential()
 
         # 1st Convolutional Layer
         model.add(Conv2D(filters=48, input_shape=(32, 32, 3), kernel_size=(8, 8), \
                          strides=(3, 3), padding='valid'))
-        model.add(Activation('relu'))
+        model.add(Activation(activation))
         # Pooling
         model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1), padding='valid'))
         # Batch Normalisation before passing it to the next layer
@@ -44,7 +48,7 @@ class HwAlexNet:
 
         # 2nd Convolutional Layer
         model.add(Conv2D(filters=126, kernel_size=(2, 2), strides=(1, 1), padding='valid'))
-        model.add(Activation('relu'))
+        model.add(Activation(activation))
         # Pooling
         model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1), padding='valid'))
         # Batch Normalisation
@@ -52,19 +56,19 @@ class HwAlexNet:
 
         # 3rd Convolutional Layer
         model.add(Conv2D(filters=192, kernel_size=(3, 3), strides=(1, 1), padding='valid'))
-        model.add(Activation('relu'))
+        model.add(Activation(activation))
         # Batch Normalisation
         model.add(BatchNormalization())
 
         # 4th Convolutional Layer
         model.add(Conv2D(filters=192, kernel_size=(2, 2), strides=(1, 1), padding='valid'))
-        model.add(Activation('relu'))
+        model.add(Activation(activation))
         # Batch Normalisation
         model.add(BatchNormalization())
 
         # 5th Convolutional Layer
         model.add(Conv2D(filters=128, kernel_size=(2, 2), strides=(1, 1), padding='valid'))
-        model.add(Activation('relu'))
+        model.add(Activation(activation))
         # Pooling
         model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1), padding='valid'))
         # Batch Normalisation
@@ -74,27 +78,27 @@ class HwAlexNet:
         model.add(Flatten())
         # 1st Dense Layer
         model.add(Dense(1024))     #, input_shape=(224 * 224 * 3,)
-        model.add(Activation('relu'))
+        model.add(Activation(activation))
         # Add Dropout to prevent overfitting
-        model.add(Dropout(0.1))
+        model.add(Dropout(0.25))
         # Batch Normalisation
-        #model.add(BatchNormalization())
+        ##aa model.add(BatchNormalization())
 
         # 2nd Dense Layer
         model.add(Dense(1024))
-        model.add(Activation('relu'))
+        model.add(Activation(activation))
         # Add Dropout
-        model.add(Dropout(0.1))
+        model.add(Dropout(0.25))
         # Batch Normalisation
-        #model.add(BatchNormalization())
+        ##aa model.add(BatchNormalization())
 
         # 3rd Dense Layer
         #model.add(Dense(1000))
-        #model.add(Activation('relu'))
+        #model.add(Activation(activation))
         # Add Dropout
         #model.add(Dropout(0.4))
         # Batch Normalisation
-        #model.add(BatchNormalization())
+        ##aa model.add(BatchNormalization())
 
         # Output Layer
         model.add(Dense(10))
